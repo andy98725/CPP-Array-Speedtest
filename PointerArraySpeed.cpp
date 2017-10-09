34e062793142aba3464d17339d93ee9c3dd4ef51
andy98725 <andyhudson725@gmail.com>
2017-10-09 03:56:19 -0500

    Elaborated; Commented code

--- PointerArraySpeed.cpp
+++ PointerArraySpeed.cpp
@@ -8,11 +8,13 @@ using namespace std;
 
 
 /********************\
-This program is a basic speedtest designed to test operations using array values vs reading the data directly.
-It uses O(n^2) operations for array size n.
+This program is a basic speedtest designed to test operations using array indicies vs reading the data directly.
+It performs O(n^2) operations for array size n using each method of accessing the data.
 It then logs the clock time spent on the operations.
 \********************/
 
+
+//Performs operations using array index
 void arrayTest (long len){
     long arr [len];
     for(long i = 0; i < len; i++){
@@ -22,6 +24,7 @@ void arrayTest (long len){
         }
     }
 }
+//Performs operations using pointer
 void pointerTest(long len){
     long arr [len];
     long * poi;
@@ -57,24 +60,39 @@ string MatchUnit(double millis){
     return ret;
 }
 
+//Main loop
 int main(){
+    //Get ticks/sec
     LARGE_INTEGER PerSec;
     QueryPerformanceFrequency(&PerSec);
+    //Log ticks/sec
     cout << "Program init." << endl << "Ticks/sec: " << PerSec.QuadPart << endl;
+    
+    //Each array size to check and log:
     long arr [] = {1,10,100,1000, 5000,10000,30000,50000,70000};
+    
+    //Measures clock time
     LARGE_INTEGER start, fin;
+    //Stores clock time
     double ticks, millis;
+    //Main loop for array sizes
     for (long i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+        //Log array size
         cout << endl << "Iterations: " << arr[i] << endl << endl;
 
+        //Perform array operations; Log start and end time
         QueryPerformanceCounter(&start);
         arrayTest(arr[i]);
         QueryPerformanceCounter(&fin);
 
+        //Find time taken
         ticks = (fin.QuadPart - start.QuadPart);
+        //Convert to milliseconds
         millis = ticks * 1000.0 / PerSec.QuadPart;
+        //Log ticks and time with matching unit
         cout << "Array time: " << (ticks) << " ticks and " << MatchUnit(millis) << endl;
 
+        //Repeat with pointer operations
         QueryPerformanceCounter(&start);
         pointerTest(arr[i]);
         QueryPerformanceCounter(&fin);
@@ -84,5 +102,6 @@ int main(){
         cout << "Pointer time: " << (ticks) << " ticks and " << MatchUnit(millis) << endl;
 
     }
+    //fin
     return 0;
 }
